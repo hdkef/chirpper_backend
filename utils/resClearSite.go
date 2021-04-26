b9322f783b4307b8965ec6fd56eefaa080b0fd45
Hadekha Erfadila Fitra <hdkef11@gmail.com>
2021-04-26 16:48:41 +0800

    set header to CLEARBEARER

--- utils/resClearSite.go
+++ utils/resClearSite.go
@@ -3,5 +3,6 @@ package utils
 import "net/http"
 
 func ResClearSite(res *http.ResponseWriter) {
-	(*res).Header().Set("Clear-Site-Data", "storage")
+	(*res).Header().Set("Access-Control-Expose-Headers", "CLEARBEARER")
+	(*res).Header().Set("CLEARBEARER", "storage")
 }
