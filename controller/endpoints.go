6d6504b6a70f3dc015d843d7d69507fe30ccee7d
Hadekha Erfadila Fitra <hdkef11@gmail.com>
2021-04-30 13:27:28 +0800

    add postFromServer method

--- controller/endpoints.go
+++ controller/endpoints.go
@@ -125,29 +125,44 @@ func broadcastNewFeed(payload MsgPayload) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	go func() {
-		defer cancel()
-		for _, v := range followers {
-			fmt.Println("send fol ID to channel")
-			folIDChan <- v.(string)
-		}
-	}()
+	go sendUpdate(followers, cancel, folIDChan)
+	go receiveUpdate(ctx, folIDChan, payload)
 
-	go func() {
-		defer func() {
-			close(folIDChan)
-		}()
-		for {
-			select {
-			case ID := <-folIDChan:
-				fmt.Println("add feed to fol ID ", ID)
-			case <-ctx.Done():
-				fmt.Println("no more fol ID")
-				return
-			}
-		}
-	}()
+}
 
+//sendUpdate will send follower ID to channel
+func sendUpdate(followers []interface{}, cancel context.CancelFunc, folIDChan chan string) {
+	defer cancel()
+	for _, v := range followers {
+		fmt.Println("send fol ID to channel", v.(string))
+		folIDChan <- v.(string)
+	}
+}
+
+//receiveUpdate receive follower ID and send payload to correspondingID
+func receiveUpdate(ctx context.Context, folIDChan chan string, payload MsgPayload) {
+	defer func() {
+		fmt.Println("channel closed")
+		close(folIDChan)
+	}()
+	for {
+		select {
+		case ID := <-folIDChan:
+			sendPayload(ID, payload)
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+//sendPayload will send payload to online user
+func sendPayload(folID string, payload MsgPayload) {
+	ws, res := onlineMap[folID]
+	if res != true || ws == payload.Conn {
+		return
+	}
+	fmt.Println("found online user, writing json..")
+	ws.WriteJSON(payload)
 }
 
 //readMsg is to read incoming msg from client, each ws.conn be assigned one readMsg goroutine
